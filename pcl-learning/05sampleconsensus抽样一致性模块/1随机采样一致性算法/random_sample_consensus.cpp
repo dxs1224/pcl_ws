b9077299f551c4e4a728963d9032d8c1d5905232
dxs <dxs@1270964478@qq.com>
2025-09-17 23:43:41 +0800

    study on 2025.9.17

--- pcl-learning/05sampleconsensus抽样一致性模块/1随机采样一致性算法/random_sample_consensus.cpp
+++ pcl-learning/05sampleconsensus抽样一致性模块/1随机采样一致性算法/random_sample_consensus.cpp
@@ -25,6 +25,7 @@
 #include <pcl/sample_consensus/sac_model_sphere.h>
 #include <pcl/visualization/pcl_visualizer.h>
 #include <boost/thread/thread.hpp>
+#include <chrono>
 
 boost::shared_ptr<pcl::visualization::PCLVisualizer>
 simpleVis(pcl::PointCloud<pcl::PointXYZ>::ConstPtr cloud, pcl::PointCloud<pcl::PointXYZ>::ConstPtr final)
@@ -36,7 +37,7 @@ simpleVis(pcl::PointCloud<pcl::PointXYZ>::ConstPtr cloud, pcl::PointCloud<pcl::P
   viewer->setBackgroundColor(0, 0, 0); // 背景颜色
 
   pcl::visualization::PointCloudColorHandlerCustom<pcl::PointXYZ> originColorHandler(cloud, 255, 255, 255); // 原始点云颜色,白色
-  pcl::visualization::PointCloudColorHandlerCustom<pcl::PointXYZ> finalColorHandler(final, 0, 255, 0); // 分割后的局内点颜色,绿色
+  pcl::visualization::PointCloudColorHandlerCustom<pcl::PointXYZ> finalColorHandler(final, 0, 255, 0);      // 分割后的局内点颜色,绿色
 
   viewer->addPointCloud<pcl::PointXYZ>(cloud, originColorHandler, "sample cloud");
   viewer->setPointCloudRenderingProperties(pcl::visualization::PCL_VISUALIZER_POINT_SIZE, 3, "sample cloud"); // 点的大小
@@ -57,39 +58,48 @@ simpleVis(pcl::PointCloud<pcl::PointXYZ>::ConstPtr cloud, pcl::PointCloud<pcl::P
  *****************************************************************************************************************/
 int main(int argc, char **argv)
 {
+  // 记录开始时间点
+  auto start = std::chrono::high_resolution_clock::now();
+
   // 初始化点云对象
   pcl::PointCloud<pcl::PointXYZ>::Ptr cloud(new pcl::PointCloud<pcl::PointXYZ>); // 存储源点云
   pcl::PointCloud<pcl::PointXYZ>::Ptr final(new pcl::PointCloud<pcl::PointXYZ>); // 存储提取的局内点
 
   // 填充点云数据
-  cloud->width = 500; // 填充点云数目
-  cloud->height = 1;  // 无序点云
-  cloud->is_dense = false;
-  cloud->points.resize(cloud->width * cloud->height);
-  for (size_t i = 0; i < cloud->points.size(); ++i)
+  if (pcl::io::loadPCDFile("/home/dxs/output.pcd", *cloud) < 0)
   {
-    if (pcl::console::find_argument(argc, argv, "-s") >= 0 || pcl::console::find_argument(argc, argv, "-sf") >= 0)
-    {
-      // 根据命令行参数用x^2+y^2+Z^2=1设置一部分点云数据，此时点云组成1/4个球体作为内点
-      cloud->points[i].x = 1024 * rand() / (RAND_MAX + 1.0);
-      cloud->points[i].y = 1024 * rand() / (RAND_MAX + 1.0);
-      if (i % 5 == 0)
-        cloud->points[i].z = 1024 * rand() / (RAND_MAX + 1.0); // 此处对应的点为局外点
-      else if (i % 2 == 0)
-        cloud->points[i].z = sqrt(1 - (cloud->points[i].x * cloud->points[i].x) - (cloud->points[i].y * cloud->points[i].y));
-      else
-        cloud->points[i].z = -sqrt(1 - (cloud->points[i].x * cloud->points[i].x) - (cloud->points[i].y * cloud->points[i].y));
-    }
-    else
-    { // 用x+y+z=1设置一部分点云数据，此时地拿云组成的菱形平面作为内点
-      cloud->points[i].x = 1024 * rand() / (RAND_MAX + 1.0);
-      cloud->points[i].y = 1024 * rand() / (RAND_MAX + 1.0);
-      if (i % 2 == 0)
-        cloud->points[i].z = 1024 * rand() / (RAND_MAX + 1.0); // 对应的局外点
-      else
-        cloud->points[i].z = -1 * (cloud->points[i].x + cloud->points[i].y);
-    }
+    std::cout << "Error loading point cloud " << std::endl
+              << std::endl;
+    return -1;
   }
+  // cloud->width = 500; // 填充点云数目
+  // cloud->height = 1;  // 无序点云
+  // cloud->is_dense = false;
+  // cloud->points.resize(cloud->width * cloud->height);
+  // for (size_t i = 0; i < cloud->points.size(); ++i)
+  // {
+  //   if (pcl::console::find_argument(argc, argv, "-s") >= 0 || pcl::console::find_argument(argc, argv, "-sf") >= 0)
+  //   {
+  //     // 根据命令行参数用x^2+y^2+Z^2=1设置一部分点云数据，此时点云组成1/4个球体作为内点
+  //     cloud->points[i].x = 1024 * rand() / (RAND_MAX + 1.0);
+  //     cloud->points[i].y = 1024 * rand() / (RAND_MAX + 1.0);
+  //     if (i % 5 == 0)
+  //       cloud->points[i].z = 1024 * rand() / (RAND_MAX + 1.0); // 此处对应的点为局外点
+  //     else if (i % 2 == 0)
+  //       cloud->points[i].z = sqrt(1 - (cloud->points[i].x * cloud->points[i].x) - (cloud->points[i].y * cloud->points[i].y));
+  //     else
+  //       cloud->points[i].z = -sqrt(1 - (cloud->points[i].x * cloud->points[i].x) - (cloud->points[i].y * cloud->points[i].y));
+  //   }
+  //   else
+  //   { // 用x+y+z=1设置一部分点云数据，此时地拿云组成的菱形平面作为内点
+  //     cloud->points[i].x = 1024 * rand() / (RAND_MAX + 1.0);
+  //     cloud->points[i].y = 1024 * rand() / (RAND_MAX + 1.0);
+  //     if (i % 2 == 0)
+  //       cloud->points[i].z = 1024 * rand() / (RAND_MAX + 1.0); // 对应的局外点
+  //     else
+  //       cloud->points[i].z = -1 * (cloud->points[i].x + cloud->points[i].y);
+  //   }
+  // }
 
   std::vector<int> inliers; // 存储局内点集合的点的索引的向量
 
@@ -117,13 +127,23 @@ int main(int argc, char **argv)
   // 复制估算模型的所有的局内点到final中
   pcl::copyPointCloud<pcl::PointXYZ>(*cloud, inliers, *final);
 
-  // 创建可视化对象并加入原始点云或者所有的局内点
+  // 计算并打印运行时间
+  auto end = std::chrono::high_resolution_clock::now();
+  auto duration = std::chrono::duration<double>(end - start);
+  std::cout << "Random Sample Consensus time: " << duration.count() << " double" << std::endl;
 
+  // 创建可视化对象并加入原始点云或者所有的局内点
   boost::shared_ptr<pcl::visualization::PCLVisualizer> viewer;
   // if (pcl::console::find_argument(argc, argv, "-f") >= 0 || pcl::console::find_argument(argc, argv, "-sf") >= 0)
   //   viewer = simpleVis(final);
   // else
   viewer = simpleVis(cloud, final);
+
+  // 计算并打印运行时间
+  auto end2 = std::chrono::high_resolution_clock::now();
+  auto duration2 = std::chrono::duration<double>(end2 - start);
+  std::cout << "visualize time: " << duration2.count() << " double" << std::endl;
+
   while (!viewer->wasStopped())
   {
     viewer->spinOnce(100);
